eb574654e8904925013088a4eefa38fd5ebaae82
vixataaa <vmetodiev93@gmail.com>
2018-01-14 19:15:48 +0200

    Restructure folders

---
+++ Src/FinanceCalculatorWeb/Classes/LeasingCalcParams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Finance
+{
+    class LeasingCalcParams
+    {
+        /// <summary>
+        /// Цена на стоката с ДДС 
+        /// </summary>
+        public decimal? Price { get; set; }
+        /// <summary>
+        /// Първоначална вноска
+        /// </summary>
+        public decimal? InitialInstallment { get; set; }
+        /// <summary>
+        /// Период на лизинга (месеци)
+        /// </summary>
+        public int? Period { get; set; }
+        /// <summary>
+        /// Месечна вноска
+        /// </summary>
+        public decimal? MonthlyInstallment { get; set; }
+        /// <summary>
+        /// Първоначална такса за обработка валута
+        /// </summary>
+        public decimal? InitialManagementFee { get; set; }
+        /// <summary>
+        /// Указва дали Първоначална такса за обработка е процент
+        /// </summary>
+        public bool TreatInitialManagementFeeAsPercent { get; set; }
+    }
+}
