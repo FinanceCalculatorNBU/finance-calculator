7664046f362b897fbec40aa6d6e7b4d59b3d19eb
Ivelin <ivelin.s.stanchev@gmail.com>
2018-01-21 20:18:34 +0200

    Added bootstrap and a new sample theme + some UI adjustments in calculators screens

--- Src/FinanceCalculatorWeb/App_Start/BundleConfig.cs
+++ Src/FinanceCalculatorWeb/App_Start/BundleConfig.cs
@@ -22,8 +22,10 @@ namespace FinanceCalculatorWeb
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css", "~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
