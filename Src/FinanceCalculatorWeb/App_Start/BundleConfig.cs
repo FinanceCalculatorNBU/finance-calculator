185c5cce367c3fee23e160ea983bed0a912ff687
Ivelin <ivelin.s.stanchev@gmail.com>
2018-01-23 21:59:45 +0200

    Fixed bug with drawer not being able to be close, fixed footer displacement

--- Src/FinanceCalculatorWeb/App_Start/BundleConfig.cs
+++ Src/FinanceCalculatorWeb/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace FinanceCalculatorWeb
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                         "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css", "~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
